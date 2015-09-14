b1e4ff409567142780a1babc0e574e0bbfda3f2d
Benoit VALLON <benoitvallon@gmail.com>
2015-09-14 12:31:08 -0400

    Adds react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    filename: "./build/helloworld.js"
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: 'helloworld.js'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel']
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
     }]
   }
 };
