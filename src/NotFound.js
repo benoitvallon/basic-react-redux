7b79424cc7cc92f9cc46f4f35467ca3e95e5129a
Benoit VALLON <benoitvallon@gmail.com>
2015-09-15 12:22:02 -0400

    Adds 404 not found page

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <p>404 : Page not found</p>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,11 @@ import {Route} from 'react-router';
 import App from './App';
 import Home from './Home';
 import About from './About';
+import NotFound from './NotFound';
 
 export default [
   { path: '/', component: App },
   { path: '/about', component: About },
-  { path: '/home', component: Home }
+  { path: '/home', component: Home },
+  { path: '*', component: NotFound }
 ];
