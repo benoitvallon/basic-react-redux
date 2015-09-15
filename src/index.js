05295102e0d3ed5837cf287c386f3ababc240c38
Benoit VALLON <benoitvallon@gmail.com>
2015-09-15 12:21:49 -0400

    Adds browsing history

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@
 import React from 'react';
 import { Router, Route, Link } from 'react-router';
 
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
 
 // We render a <Router> with some <Route>s.
 // It does all the fancy routing stuff for us.
 React.render(
-  <Router routes={routes}/>,
+  <Router routes={routes} history={createBrowserHistory()}/>,
   document.body
 )
